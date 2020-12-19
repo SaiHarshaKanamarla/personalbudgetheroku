b4495135413ec1ad59749ea589510998fe9191d3
sidharsha <siddharth.landers@gmail.com>
2020-12-18 21:27:44 -0500

    Heroku

--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/personal-budget'));
+app.use(express.static(__dirname+'/personal-budget'));
 
 app.get('/*',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/dist/personal-budget/index.html'));
+    res.sendFile(path.join(__dirname+'/personal-budget/index.html'));
 });
 
 app.listen(8080);
